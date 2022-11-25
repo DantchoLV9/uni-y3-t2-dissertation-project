7ae4e518a1983dd9b5b858dd50aee16a1555a545
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2022-11-25 22:18:11 +0000

    Added logo

---
+++ client/src/images/logo.js
@@ -0,0 +1,22 @@
+const Logo = () => (
+    <svg
+        version="1.0"
+        xmlns="http://www.w3.org/2000/svg"
+        width="362.000000pt"
+        height="362.000000pt"
+        viewBox="0 0 362.000000 362.000000"
+        preserveAspectRatio="xMidYMid meet">
+        <g
+            transform="translate(0.000000,362.000000) scale(0.100000,-0.100000)"
+            fill="#000000"
+            stroke="none">
+            <path
+                d="M1039 1829 c-412 -826 -748 -1508 -745 -1515 4 -12 93 -14 526 -14
+l520 0 233 465 232 465 233 -465 232 -465 520 0 c433 0 522 2 526 14 8 21
+-1493 3016 -1511 3016 -10 0 -269 -508 -766 -1501z"
+            />
+        </g>
+    </svg>
+)
+
+export default Logo
