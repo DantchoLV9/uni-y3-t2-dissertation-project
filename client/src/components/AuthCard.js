89d4b13cf15eb2028010c78b000b06944b1504b2
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2023-02-02 15:57:11 +0000

    Fixed auth pages background image responsivness

--- client/src/components/AuthCard.js
+++ client/src/components/AuthCard.js
@@ -6,13 +6,12 @@ const AuthCard = ({ logo, displayDisabledMsg, children }) => {
         process.env.NEXT_PUBLIC_DISABLE_DATA_COLLECTION === 'true'
     return (
         <>
-            {/* TODO: Make the background responsive */}
             <div className="absolute top-0 left-0 w-full h-full bg-no-repeat -z-10 flex overflow-hidden">
                 <img
-                    className="w-2/5 h-full object-cover"
+                    className="w-2/5 h-full object-cover hidden sm:inline-block"
                     src={cameraImage.src}
                 />
-                <div className="left-1/3 -skew-x-6 absolute w-full h-full overflow-hidden bg-gray-100 shadow-2xl"></div>
+                <div className="sm:left-1/3 sm:-skew-x-6 absolute w-full h-full overflow-hidden bg-gray-100 shadow-2xl"></div>
             </div>
             <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
                 <div>{logo}</div>
